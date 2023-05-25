dee60eb846c777f502fa714158dd446f77add8c8
Shehan-Lakshitha <shehanlaksh@gmail.com>
2023-05-25 13:25:36 +0530

    edited css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import {useEffect} from "react";
 
-//36f5b6e9
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=36f5b6e9';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() =>{
+        searchMovies('Spiderman');
+    }, []);
+
     return (
-        <h1>App</h1>
+        <div>
+            <h1>MovieLand</h1>
+        </div>
     );
 }
 
